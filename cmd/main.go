8deb443e60a0554aac97839d61aa3a1653d58165
Frank Vasquez <frank@customer.lan>
2024-09-26 21:06:05 -0600

    Fizz Buzz

--- cmd/main.go
+++ cmd/main.go
@@ -1,7 +1,15 @@
 package main
 
-import "fmt"
+import (
+	"algorithms-notes-for-professionals/fizzbuzz"
+	"context"
+	"fmt"
+)
 
 func main() {
-	fmt.Println("Template project")
+	fmt.Println("Fizz Buzz algorithm")
+
+	fizzBuzzAlg := fizzbuzz.New()
+	numbers := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15}
+	fizzBuzzAlg.Execute(context.Background(), numbers)
 }
--- fizzbuzz/fizzbuzz.go
+++ fizzbuzz/fizzbuzz.go
@@ -1,6 +1,9 @@
 package fizzbuzz
 
-import "context"
+import (
+	"context"
+	"fmt"
+)
 
 type FizzBuzz interface {
 	Execute(ctx context.Context, numbers []int)
@@ -9,6 +12,25 @@ type FizzBuzz interface {
 type fizzbuzz struct {
 }
 
+func New() *fizzbuzz {
+	return &fizzbuzz{}
+}
+
 func (fb fizzbuzz) Execute(ctx context.Context, numbers []int) {
+	p := fmt.Println
+	for _, number := range numbers {
+		// This is the same as number % 3 && number % 5
+		// But when we are evaluating algorithm efficiency is a key point.
+		// In this case we reduce the two operations to one, so we can say that is most efficient.
+		if number%15 == 0 {
+			p(fmt.Sprintf("%d, Fizz Buzz", number))
+		} else if number%3 == 0 {
+			p(fmt.Sprintf("%d, Fizz", number))
+		} else if number%5 == 0 {
+			p(fmt.Sprintf("%d, Buzz", number))
+		} else {
+			p(fmt.Sprintf("%d", number))
+		}
+	}
 
 }
